ff2e32d914c8e91e0e067b6f8f3e5fefbee3fce8
MichaelQuant4R <Michael.R2014@yahoo.co.uk>
2021-07-31 18:13:48 +0100

    app.py build

--- app/app.py
+++ app/app.py
@@ -3,9 +3,14 @@ from flask_cors import CORS
 import os
 
 
-app = Flask(__name__)
+app = Flask(__name__,static_folder="../build", static_url_path="/" )
 
 cors = CORS(app, support_credentials=True, origin = {'r{/api/*}'})
+
+
+@app.route('/')
+def index():
+    return app.send_static_file('index.html')
 
 
 @app.route("/api/comments", methods = ["GET"])
